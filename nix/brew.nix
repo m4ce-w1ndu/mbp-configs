3edf27ba1ac2ed4d34a6cd700ff0c961c6fbd6ea
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-01-22 17:45:58 +0100

    Added mas (Mac App Store CLI) and Mac App Store Apps (Bitwarden, Wireguard)

--- nix/brew.nix
+++ nix/brew.nix
@@ -14,6 +14,11 @@
       # "koekeishiya/formulae" # Uncomment if you use yabai/skhd later
     ];
 
+    # Brews (CLI apps)
+    brews = [
+      "mas"
+    ];
+
     # Casks (GUI Applications)
     casks = [
       "zed"
@@ -25,6 +30,12 @@
       "podman-desktop"
       "antigravity"
     ];
+
+    # Mac App Store apps
+    masApps = {
+      "Bitwarden" = 1352778147;
+      "Wireguard" = 1451685025;
+    };
 
     # If you want fonts via Brew (optional, since you have them in Nix config too)
     # Note: New naming convention often uses "font-" prefix
